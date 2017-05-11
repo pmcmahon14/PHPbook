8deb5a470a6eaa9999e36b85360ffc48b9652200
Patrick McMahon <pmcmahon14@gmail.com>
2017-05-10 19:30:03 -0700

    second section in first chapter

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <html>
 <?php
-echo("Hello World!");
+echo("Hello World! ");
+?>
+<?php
+echo ("OK");//output OK this comment is used on a single line
+echo(" Hello");/*output Hello
+this comment is used on multiple lines*/
 ?>
 </html>
