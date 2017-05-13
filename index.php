34f66bea2e8754a20296c837319e6c78e7f11617
Patrick McMahon <pmcmahon14@gmail.com>
2017-05-13 12:16:57 -0700

    first chapter done-ish

--- index.php
+++ index.php
@@ -13,4 +13,17 @@ this comment is used on multiple lines*/
 echo "PHP said \"Hello World!\"\r";
 echo "Hello \t\t\t World!";
 ?>
+<?php
+$num = 200;
+function test() {
+    $num = 100;
+}echo $num;
+?>
+<?php
+function sum($a, $b, $c) {
+    $sum = $a+$b+$c;
+    echo $sum;
+}
+sum(3, 6, 9);
+?>
 </html>
