6ae976eda2e50b81749e68eae32ae00c6369652e
Patrick McMahon <pmcmahon14@gmail.com>
2017-05-13 13:45:02 -0700

    put each result on separate line

--- index.php
+++ index.php
@@ -7,18 +7,22 @@ echo ("OK");//output OK this comment is used on a single line
 echo(" Hello");/*output Hello
 this comment is used on multiple lines*/
 ?>
-</html>
+</html><br>
 <html>
 <?php
 echo "PHP said \"Hello World!\"\r";
 echo "Hello \t\t\t World!";
 ?>
+</html><br>
+<html>
 <?php
 $num = 200;
 function test() {
     $num = 100;
 }echo $num;
 ?>
+</html><br>
+<html>
 <?php
 function sum($a, $b, $c) {
     $sum = $a+$b+$c;
