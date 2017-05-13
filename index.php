e15b3f4d7a7b1bc53b487d821596999d5b814b45
Patrick McMahon <pmcmahon14@gmail.com>
2017-05-13 14:37:55 -0700

    assignment chapter 1

--- index.php
+++ index.php
@@ -1,33 +1,14 @@
 <html>
 <?php
-echo("Hello World! ");
-?>
-<?php
-echo ("OK");//output OK this comment is used on a single line
-echo(" Hello");/*output Hello
-this comment is used on multiple lines*/
-?>
-</html><br>
-<html>
-<?php
-echo "PHP said \"Hello World!\"\r";
-echo "Hello \t\t\t World!";
-?>
-</html><br>
-<html>
-<?php
-$num = 200;
-function test() {
-    $num = 100;
-}echo $num;
-?>
-</html><br>
-<html>
-<?php
-function sum($a, $b, $c) {
-    $sum = $a+$b+$c;
-    echo $sum;
+function multiply($x, $y) {
+    calculate($x, $y);
 }
-sum(3, 6, 9);
+function calculate ($a, $b){
+    $result = $a * $b;
+    echo ("The value is $result");
+}
+?>
+<?php
+multiply (10, 20);
 ?>
 </html>
