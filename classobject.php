4d3f135574d1f5735e08499d987982c1ed18fb19
Patrick McMahon <pmcmahon14@gmail.com>
2017-05-14 17:15:33 -0700

    finished classobject assignment but problem with C icon on file

--- classobject.php
+++ classobject.php
@@ -1,1 +1,21 @@
+<html>
 <?php
+class setDate { //define a class "setDate"
+    var $day;  //declare three variable members
+    var $month;
+    var $year;
+    function construct($m,$d,$y) {  //constructor
+        $this->month = $m;  //initialization
+        $this->day = $d;
+        $this->year = $y;
+        echo "Constructor is called!<br><br>";
+        echo "The date is:".$this->month."/".$this->day."/".$this->year;
+        echo "<br><br>";
+    }
+    function destruct(){  //destructor
+        echo "Destructor is called!";
+    }
+}
+$obj = new setDate("12","25","2016");  /*create an object and call constructor automatically*/
+?>
+</html>
